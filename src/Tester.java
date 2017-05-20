48a42774d8bd26aee5c93457f161271a48382426
Shubh Sinha <sinha.shubhankar@gmail.com>
2017-05-20 00:32:19 -0500

    manually converting numbers to binary with use of scanner

---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+    int x;
+    static int y;
+
+    public static void main(String[] args) {
+
+        for (double d = 1/8; d < 1; d = d* 2) {
+            //Do something
+            System.out.println("hi");
+        }
+    }
+}
